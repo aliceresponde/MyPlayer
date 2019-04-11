bac5456a5881932fa20d3675396987a385da169b
Alicia Beltran <alicia.beltran@carnival.com>
2019-04-10 23:45:35 -0500

    creo  tipo de media item PHOTO, VIDEO mediante enums

--- app/src/main/java/com/aliceresponde/myplayer/MediaItem.kt
+++ app/src/main/java/com/aliceresponde/myplayer/MediaItem.kt
@@ -1,3 +1,6 @@
 package com.aliceresponde.myplayer
 
-data class MediaItem (val title: String , val iconUrl: String)
+data class MediaItem (val title: String , val iconUrl: String,
+                      val type : Type){
+    enum class Type {PHOTO, VIDEO}
+}
