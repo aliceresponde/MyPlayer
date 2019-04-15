4def19a62f49574aeae159ad302f774fb1e94d14
Alicia Beltran <alicia.beltran@carnival.com>
2019-04-15 14:20:09 -0500

    use kotlin extensions on Recyclerview to access to xml elements using itemView

--- app/src/main/java/com/aliceresponde/myplayer/MainActivity.kt
+++ app/src/main/java/com/aliceresponde/myplayer/MainActivity.kt
@@ -2,24 +2,16 @@ package com.aliceresponde.myplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toast("Hi Kotlin")
         main_textV.text = "-- Kotlin --"
+        recycler.adapter = MediaAdapter(emptyList())
         main_show_btn.setOnClickListener { toast(message = "Good Morning ${main_editText.text}") }
     }
-
-    fun toast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
 }
--- app/src/main/java/com/aliceresponde/myplayer/MediaAdapter.kt
+++ app/src/main/java/com/aliceresponde/myplayer/MediaAdapter.kt
@@ -1,11 +1,9 @@
 package com.aliceresponde.myplayer
 
-import android.opengl.Visibility
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view_media_item.view.*
 
 class MediaAdapter(val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
 
@@ -22,22 +20,16 @@ class MediaAdapter(val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapt
     override fun getItemCount(): Int = items.size
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        //        val mediaItemTile = view.findViewById(R.id.media_item_title) as TextView
         //        val mediaItemPic = view.findViewById(R.id.media_item_pic) as ImageView
-        val mediaItemTile = view.find<TextView>(R.id.media_item_title)
-        val mediaItemPic = view.find<ImageView>(R.id.media_item_pic)
-        val mediaVideoIndicator = view.find<ImageView>(R.id.media_video_indicator)
-        fun bind(item: MediaItem) {
+        //        val mediaItemPic = view.find<ImageView>(R.id.media_item_pic)
 
-            mediaVideoIndicator.visibility = when (item.type) {
+        fun bind(item: MediaItem) {
+            itemView.media_item_title.text = item.title
+            itemView.media_item_pic.loadURL(item.iconUrl)
+            itemView.media_video_indicator.visibility = when (item.type) {
                 MediaItem.Type.PHOTO -> View.GONE
                 MediaItem.Type.VIDEO -> View.VISIBLE
             }
-
-            mediaItemTile.text = item.title
-//            Picasso.with(mediaItemPic.context).load(item.iconUrl).into(mediaItemPic)
-            mediaItemPic.loadURL(item.iconUrl)
-            toast("hello")
         }
     }
 }
